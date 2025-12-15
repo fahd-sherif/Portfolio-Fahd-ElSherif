86157de98d8fb563c9a1a265c778199b1d0d0a27
Fahd <fahdsherif2005@gmail.com>
2025-12-15 21:41:52 +0200

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ const projectData = [
   {
     id: 1,
     image: card1,
-    category: "Dashboard",
+    category: "Dashboard (Power BI)",
     title: "retail_sales_2024",
     description:
       "A focused analysis of retail sales and profit metrics, providing insight into regional and category performance.",
@@ -16,21 +16,21 @@ const projectData = [
   {
     id: 2,
     image: card2,
-    category: "Dashboard",
-    title: "CA Sales",
-    description:
-      "This presents key revenue trends and sales performance across different provinces and categories.",
-    link: "https://app.powerbi.com/view?r=eyJrIjoiZmU1NDM3ZTYtMzFlNi00MzA4LThjOTQtYWZmNDg0ZDhjMzM5IiwidCI6IjJiYjZlNWJjLWMxMDktNDdmYi05NDMzLWMxYzZmNGZhMzNmZiIsImMiOjl9",
-  },
-  {
-    id: 3,
-    image: card3,
-    category: "Dashboard",
+    category: "Dashboard (Power BI)",
     title: "Sample - Superstore 2019",
     description:
       "Delivering a comprehensive analysis on Superstore sales, profit, and order metrics across various segments and regions effectively.",
     link: "https://app.fabric.microsoft.com/view?r=eyJrIjoiNzc2MGQ3NDItNTQzZC00NTE4LTk5MTAtOWNlYmNjZGFlYTc4IiwidCI6IjJiYjZlNWJjLWMxMDktNDdmYi05NDMzLWMxYzZmNGZhMzNmZiIsImMiOjl9",
   },
+  {
+    id: 3,
+    image: card3,
+    category: "Dashboard (Power BI)",
+    title: "UK Train Rides",
+    description:
+      "Delivering a comprehensive analysis of train trips, revenue, punctuality, and delays across different stations and regions effectively.",
+    link: "https://app.fabric.microsoft.com/view?r=eyJrIjoiYzMwNjlhZmMtNDI0OS00ZDBiLTg3OGMtMzM0ZWRkYzhlODVkIiwidCI6IjJiYjZlNWJjLWMxMDktNDdmYi05NDMzLWMxYzZmNGZhMzNmZiIsImMiOjl9",
+  }
 ];
 
 const Portfolio = () => {
